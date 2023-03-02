a5a2a8dcccef9494c8cb7915f8999ee5b011137e
Douglas Uba <douglas.uba@inpe.br>
2023-03-02 16:30:12 -0300

    Add goes2image banner

---
+++ goes2image/banner.py
@@ -0,0 +1,19 @@
+#
+# This file is part of goes2image utility.
+# Copyright (C) 2023 INPE.
+#
+# goes2image is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+
+__author__ = 'Douglas Uba'
+__email__  = 'douglas.uba@inpe.br'
+
+GOES2IMAGE = '''
+█▀▀ █▀█ █▀▀ █▀ ▀█ █ █▀▄▀█ ▄▀█ █▀▀ █▀▀
+█▄█ █▄█ ██▄ ▄█ █▄ █ █░▀░█ █▀█ █▄█ ██▄
+> A tool to convert GOES/ABI data to well-known image format.
+🛰️ Division of Satellites and Meteorological Sensors - DISSM.
+🌎 National Institute for Space Research - INPE, Brazil.
+📧 Contact: Douglas Uba - douglas.uba@inpe.br
+'''
