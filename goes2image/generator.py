ba34a227968fe4b89f9d0782c2a130d033918d1c
Douglas Uba <douglas.uba@inpe.br>
2023-03-02 16:39:02 -0300

    Add main script file and image generator

---
+++ goes2image/generator.py
@@ -0,0 +1,12 @@
+#
+# This file is part of goes2image utility.
+# Copyright (C) 2023 INPE.
+#
+# goes2image is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+
+__author__ = 'Douglas Uba'
+__email__  = 'douglas.uba@inpe.br'
+
+# TODO
