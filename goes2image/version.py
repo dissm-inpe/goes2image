a91db2033ea0e39e027ee674431d196c75a244e0
Douglas Uba <douglas.uba@inpe.br>
2023-03-02 16:27:35 -0300

    Start package

---
+++ goes2image/version.py
@@ -0,0 +1,14 @@
+#
+# This file is part of goes2image utility.
+# Copyright (C) 2023 INPE.
+#
+# goes2image is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+
+__author__ = 'Douglas Uba'
+__email__  = 'douglas.uba@inpe.br'
+
+"""Version information for goes2image utility."""
+
+__version__ = '1.0.0'
