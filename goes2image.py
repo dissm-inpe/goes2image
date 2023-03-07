cac7a475c13dc34017b933b671e82c24703a1ea0
Douglas Uba <douglas.uba@inpe.br>
2023-03-07 10:14:31 -0300

    Add tool implementation

--- goes2image.py
+++ goes2image.py
@@ -6,4 +6,90 @@
 # under the terms of the MIT License; see LICENSE file for more details.
 #
 
-# TODO
+import argparse
+
+import matplotlib
+matplotlib.use('agg')
+
+import goes2image
+import goes2image.banner
+from goes2image import generator
+from goes2image.config import Config
+
+if __name__ == '__main__':
+    # Hello, my friend!
+    print(goes2image.banner.GOES2IMAGE)
+
+    # Create command-line parser
+    parser = argparse.ArgumentParser(
+        description='A tool to convert GOES/ABI data to well-known image format.', prog='goes2image'
+    )
+    # Create command-line parameters
+    #> Input file
+    parser.add_argument('--input', '-i', help='Path to GOES-16 netCDF file', type=str,
+        dest='input', required=True)
+    #> Product
+    parser.add_argument('--product', '-p', help='You can use default product configuration. Choose one from the list', type=str, dest='product', choices=Config.getProducts(), default=None, required=False)
+    #> netCDF variable
+    parser.add_argument('--variable', '-var', help='Variable that will be used (e.g. CMI)', type=str,
+        dest='var', default='CMI', required=False)
+    #> Number of Blocks
+    parser.add_argument('-nblocks', help='Number of blocks that will be used to plot image', type=int,
+        dest='nblocks', default=4, required=False)
+    #> Min value
+    parser.add_argument('-vmin', help='Define the data range (min) that the colormap covers', type=float,
+        dest='vmin', default=None, required=False)
+    #> Max value
+    parser.add_argument('-vmax', help='Define the data range (max) that the colormap covers', type=float,
+        dest='vmax', default=None, required=False)
+     #> No-data value
+    parser.add_argument('-nodata', help='Define the no-data value', type=float, dest='nodata',
+        default=None, required=False)
+    #> Colormap
+    parser.add_argument('--colormap', '-cmap', help='Color map that will be used', type=str,
+        dest='cmap', default='gray', required=False)
+    #> Output number of bands (use for color images, for example)
+    parser.add_argument('-nbands', help='Number of bands of image result', type=int,
+        dest='nbands', default=1, required=False)
+    #> Auto-scale
+    parser.add_argument('--autoscale', help='Flag that indicates if the netCDF driver will call \'set_auto_scale\' for all variables', action='store_true', dest='autoscale')
+    #> Resize factor
+    parser.add_argument('-resize', help='Optional size that can be used to resize data', type=int,
+        dest='resize', default=None, required=False)
+    #> Extent (for remap operation)
+    parser.add_argument('--extent', help='Optional extent for remap process', nargs=4,
+        metavar=('llx', 'lly', 'urx', 'ury'), type=float, dest='extent', default=None, required=False)
+    #> Resolution (for remap operation)
+    parser.add_argument('--resolution', '-res', help='Output resolution (remap use)', type=int, default=2)
+     #> Output filename
+    parser.add_argument('--output', '-o', help='Path to output file that will be generated',
+        type=str, dest='output', required=True)
+    #> Output driver
+    parser.add_argument('--driver', '-d', help='GDAL driver name that will be used to generate final result',
+        type=str, default='PNG', dest='driver', required=False)
+     #> Version
+    parser.add_argument('--version', '-v', action='version', version=goes2image.__version__)
+
+    # Parse input
+    args = parser.parse_args()
+
+    if args.product: # Use default config?
+        product = Config.getProduct(args.product)
+        args.var = product['var']
+        args.nblocks = product['nblocks']
+        args.vmin = product['vmin']
+        args.vmax = product['vmax']
+        args.cmap = product['cmap']
+        args.nbands = product['nbands']
+        args.autoscale = product['autoscale']
+
+    # Show some infos
+    print('-- Parameters --')
+    for arg in vars(args):
+        print('* ' + arg + ':', getattr(args, arg))
+
+    # Build image!
+    generator.run(args.input, args.var,
+        args.vmin, args.vmax, args.nodata, args.cmap,
+        args.output, args.nbands, args.nblocks, args.autoscale, args.resize,
+        args.extent, args.resolution, args.driver)
--- goes2image/generator.py
+++ goes2image/generator.py
@@ -6,4 +6,186 @@
 # under the terms of the MIT License; see LICENSE file for more details.
 #
 
-# TODO
+import multiprocessing
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+from netCDF4 import Dataset
+from osgeo import gdal
+from PIL import Image
+
+from goes2image import fig2np
+from goes2image.constants import LAT_LON_WGS84
+from goes2image.remap import getGeoT, sat2grid
+from goes2image.utils import LoadCPT
+
+def draw2result(fig, block, line, raster, vmin=None, vmax=None, cmap='gray'):
+    # Remove axes
+    ax = plt.Axes(fig, [0.0, 0.0, 1.0, 1.0], facecolor='black')
+    ax.set_axis_off()
+    fig.add_axes(ax)
+
+    # Plot data block
+    plt.imshow(block, vmin=vmin, vmax=vmax, cmap=cmap, interpolation=None)
+
+    # Convert result to numpy array
+    if(raster.RasterCount == 1):
+        result = fig2np.gray(fig)
+    elif(raster.RasterCount == 3):
+        result = fig2np.rgb(fig)
+    else:
+        result = fig2np.rgba(fig)
+
+    # Verify bounds based on current line and block
+    limit = result.shape[0]
+    if line + result.shape[0] > raster.RasterYSize:
+        limit = limit - (line + result.shape[0] - raster.RasterYSize)
+        
+    # Write block to final result
+    if(raster.RasterCount == 1):
+        raster.GetRasterBand(1).WriteArray(result[0:limit,:], 0, line)
+    else:
+        for i in range(0, raster.RasterCount):
+            raster.GetRasterBand(i+1).WriteArray(result[0:limit,:,i], 0, line)
+
+    # Clear plot and prepare next
+    plt.clf()
+
+def create_result(path, nlines, ncols, nbands=1, driver='GTiff', options=[]):
+    driver = gdal.GetDriverByName(driver)
+    image = driver.Create(path, ncols, nlines, nbands, gdal.GDT_Byte, options)
+    return image
+
+def create_plot(dim):
+    dpi = 100.0
+    npxl_x = dim[1]
+    npxl_y = dim[0] + 0.5
+    fig = plt.figure(figsize=((npxl_x/float(dpi)), (npxl_y/float(dpi))), frameon=False, dpi=dpi, facecolor='black')
+    return fig
+
+def process_block(lock, block, line, i, vmin, vmax, cmap, fulldisk_path):
+    # Feedback info
+    print('Processing block {}.'.format(i))
+
+    # Create plot
+    fig = create_plot(block.shape)
+
+    #lock.acquire()
+
+    try:
+        # Open to update
+        fulldisk = gdal.Open(fulldisk_path, gdal.GA_Update)
+        print('Writing block {}.'.format(i))
+        # Plot block
+        draw2result(fig, block, line, fulldisk, vmin=vmin, vmax=vmax, cmap=cmap)
+        # Close to flush
+        fulldisk = None
+    finally:
+        #lock.release()
+        fig.clf()
+
+def run(input, var, vmin, vmax, nodata, cmap, output, nbands, nblocks,
+        autoscale, resize=None, extent=None, resolution=None, outDriver='JPEG'):
+    # Open netCDF
+    nc = Dataset(input)
+    nc.set_auto_scale(autoscale) # Tip: if False, use int16 array in memory
+
+    # Get scale and offset factors
+    if(autoscale == False):
+        scale = nc.variables[var].scale_factor
+        offset = nc.variables[var].add_offset
+
+    # Load CPT colorbar, if it is a file
+    if(os.path.isfile(cmap)):
+       cmap = LoadCPT(cmap)
+
+    # Transform vmin/vmax to in16, if necessary
+    if(vmin is not None and autoscale == False):
+        vmin = int((vmin - offset)/scale)
+
+    if(vmax is not None and autoscale == False):
+        vmax = int((vmax - offset)/scale)
+
+    # Load data
+    if extent is None:
+        data = nc.variables[var][:] # satellite-view
+    else:
+        # Close current netCDF in order to open on remap process
+        nc.close()
+        # Remap
+        print('Remapping...')
+        grid = sat2grid(input, extent, resolution, LAT_LON_WGS84, 'HDF5',
+            autoscale=False, var=var, progress=gdal.TermProgress_nocb)
+        data = grid.ReadAsArray()
+        grid = None
+        # Open again...
+        nc = Dataset(input)
+        nc.set_auto_scale(autoscale)
+
+    # Apply no-data mask if requested
+    if nodata is not None:
+        data = np.ma.masked_where(data == nodata, data)
+
+    # Resize data, if requested
+    if resize:
+        data = np.ma.filled(data, nc.variables[var]._FillValue)
+        data = np.array(Image.fromarray(data).resize((resize, resize), Image.NEAREST))
+        data = np.ma.masked_where(data == nc.variables[var]._FillValue, data)
+
+    # Get dimensions
+    nlines = data.shape[0]
+    ncols  = data.shape[1]
+
+    # Create result
+    tif_output = output + '.temp.tif'
+    result = create_result(tif_output, nlines, ncols, nbands=nbands)
+
+    # Adjust SRS, if necessary
+    if extent:
+        result.SetProjection(LAT_LON_WGS84.ExportToWkt())
+        result.SetGeoTransform(getGeoT(extent, result.RasterYSize, result.RasterXSize))
+
+    # Close
+    result = None
+
+    # Divide original data in n-blocks
+    blocks = np.array_split(data[:], nblocks)
+
+    # Compute lines for each block
+    lines = [b.shape[0] for b in blocks]
+    lines = [sum(lines[:i+1]) for i in range(len(lines))]
+    lines.insert(0, 0)
+    lines.pop()
+
+    # Build data package that will be processed -> (block, line, block index)
+    pkg = zip(blocks, lines, range(1, nblocks + 1))
+
+    # TODO: transform 'max_process_number' as input parameter
+    max_process_number = multiprocessing.cpu_count()
+
+    # Build composition
+    processes = []
+    lock = multiprocessing.Lock()
+    for block, line, index in pkg:
+        p = multiprocessing.Process(target=process_block,
+            args=(lock,block,line,index,vmin,vmax,cmap,tif_output,))
+        processes.append(p)
+        p.start()
+        if(len(processes) >= max_process_number):
+            # Wait each process...
+            for p in processes:
+                p.join()
+            processes = []
+
+    for p in processes:
+        p.join()
+
+    # Export to PNG
+    print('Converting to {}...'.format(outDriver))
+    tiff = gdal.Open(tif_output)
+    png = gdal.GetDriverByName(outDriver).CreateCopy(output, tiff)
+    tiff, png = None, None
+    os.remove(tif_output)
+    print('done!')
+
